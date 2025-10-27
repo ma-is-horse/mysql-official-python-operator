3d4ecd19ec06fc90977559ee68b6567622539d05
MaBing <ma_20241010@163.com>
2025-10-27 15:33:40 +0800

    更新打包脚本

--- build.mine.sh
+++ build.mine.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
-tag=mabing1024-1
-docker build -f Dockerfile.mine.dev -t release-ci.daocloud.io/demo/mgr-operator:${tag} . --push
+
+# 使用传入的第一个参数作为 tag，如果未提供则使用默认值
+tag="${1:-mabing1027}"
+
+docker build -f Dockerfile.mine.dev -t "release-ci.daocloud.io/demo/mgr-operator:${tag}" . --push
